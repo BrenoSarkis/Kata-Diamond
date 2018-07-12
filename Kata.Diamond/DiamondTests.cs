9bfd83f7bf3cb74ff9eb9aad93a69ca09e2778de
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-12 21:00:40 +0200

    Diamond of A

---
+++ Kata.Diamond/DiamondTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace Kata.Diamond
+{
+    [TestFixture]
+    public class DiamondTests
+    {
+        [Test]
+        public void DiamondOfA()
+        {
+            Assert.That(DiamondMaker.CreateFor("A"), Is.EqualTo("A"));
+        }
+    }
+
+    public class DiamondMaker
+    {
+        public static string CreateFor(string letter)
+        {
+            return "A";
+        }
+    }
+}
