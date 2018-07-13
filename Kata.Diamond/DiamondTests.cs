ce359a9549cf6073688e3afe2101c70d5660bc18
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-13 22:03:16 +0200

    Third attempt begins!

--- Kata.Diamond/DiamondTests.cs
+++ Kata.Diamond/DiamondTests.cs
@@ -15,46 +15,18 @@ namespace Kata.Diamond
     public class DiamondTests
     {
         [Test]
-        public void DiamondOfA_HasNoSpaces()
+        public void OneRowDiamond_HasOnlyOneLetter()
         {
-            Assert.That(DiamondMaker.CreateFor("A")[0], Is.EqualTo("A"));
+            Assert.That(DiamondMaker.CreateFor("A"), Is.EqualTo("A"));
         }
 
-        [Test]
-        public void DiamondOfB_FirstRowHasSurroundingSpaces()
-        {
-            var diamond = DiamondMaker.CreateFor("B");
-            Assert.That(diamond[0], Is.EqualTo(" A "));
-        }
-
-        [Test]
-        public void DiamondOfC_FirstRowHasTwoSurroundingSpaces()
-        {
-            var diamond = DiamondMaker.CreateFor("C");
-            Assert.That(diamond[0], Is.EqualTo("  A  "));
-        }
-
-        [Test]
-        public void DiamondOfB_SecondRowStartsAndEndsOnB()
-        {
-            var diamond = DiamondMaker.CreateFor("B");
-            Assert.That(diamond[1], Is.EqualTo("B B"));
-        }
     }
 
     public class DiamondMaker
     {
-        public static string[] CreateFor(string letter)
+        public static string CreateFor(string letter)
         {
-            var diamond = new List<string>();
-            var alphabet = new[] { "A", "B", "C" };
-            var letterIndex = Array.FindIndex(alphabet, l => l == letter);
-            diamond.Add(new String(' ', letterIndex) + "A" + new String(' ', letterIndex));
-            if (letterIndex > 0)
-            {
-                diamond.Add(alphabet[letterIndex] + new String(' ', letterIndex) + alphabet[letterIndex]);
-            }
-            return diamond.ToArray();
+            return "A";
         }
     }
 }
