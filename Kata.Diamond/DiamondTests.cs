affe3299086ff7a41b766f1c261eb791ad6b0917
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-18 19:42:50 +0200

    DiamondOfC_LetterAIsAtTheCenterWithSpacing

--- Kata.Diamond/DiamondTests.cs
+++ Kata.Diamond/DiamondTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
 
 namespace Kata.Diamond
 {
@@ -18,22 +20,48 @@ namespace Kata.Diamond
             var diamond = new Diamond("B");
             Assert.That(diamond.Generate(), Is.EqualTo("-A-"));
         }
+
+        [Test]
+        public void DiamondOfC_LetterAIsAtTheCenterWithSpacing()
+        {
+            var diamond = new Diamond("C");
+            Assert.That(diamond.Generate(), Is.EqualTo("--A--"));
+        }
     }
 
     public class Diamond
     {
-        private string letter;
+        private readonly string letter;
+        private readonly string[] alphabet = { "A", "B", "C" };
+        private readonly int letterIndex = 0;
+        private readonly int size = 0;
 
         public Diamond(string letter)
         {
             this.letter = letter;
+            letterIndex = Array.FindIndex(alphabet, x => x == letter);
+            size = letterIndex * 2 + 1;
         }
 
         public string Generate()
         {
-            if (letter == "B")
+            if (letterIndex > 0)
             {
-                return "-A-";
+                var diamond = "";
+                var centerOfTheDiamond = size / 2;
+                for (int i = 0; i < size; i++)
+                {
+                    if (i == centerOfTheDiamond)
+                    {
+                        diamond += "A";
+                    }
+                    else
+                    {
+                        diamond += "-";
+                    }
+                }
+
+                return diamond;
             }
             return "A";
         }
