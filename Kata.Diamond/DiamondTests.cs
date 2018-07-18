47871e3c5a6e0d96f9773538ce4660caeee0f75f
Breno Sarkis <sarkisbreno@gmail.com>
2018-07-18 19:21:35 +0200

    Diamond of A

--- Kata.Diamond/DiamondTests.cs
+++ Kata.Diamond/DiamondTests.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using NUnit.Framework.Constraints;
-using IFormatProvider = System.IFormatProvider;
+﻿using NUnit.Framework;
 
 namespace Kata.Diamond
 {
@@ -15,29 +6,24 @@ namespace Kata.Diamond
     public class DiamondTests
     {
         [Test]
-        public void SingleLetterDiamond()
+        public void DiamondOfA()
         {
-            Assert.That(DiamondMaker.CreateFor("A"), Is.EqualTo("A"));
+            var diamond = new Diamond("A");
+            Assert.That(diamond.Generate(), Is.EqualTo("A"));
         }
-
-        [Test]
-        public void CenterOfDiamondHasTwoLettersWithSpacing()
-        {
-            Assert.That(DiamondMaker.CreateFor("B"), Is.EqualTo("B-B"));
-
-        }
-
-
     }
 
-    public class DiamondMaker
+    public class Diamond
     {
-        public static string CreateFor(string letter)
+        private string letter;
+
+        public Diamond(string letter)
         {
-            if (letter == "B")
-            {
-                return "B-B";
-            }
+            this.letter = letter;
+        }
+
+        public string Generate()
+        {
             return "A";
         }
     }
